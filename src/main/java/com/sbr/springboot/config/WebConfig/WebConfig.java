dbff29fa061abe580efd2069012d149c236e65b0
boraborason <57280699+boraborason@users.noreply.github.com>
2022-12-27 21:53:48 +0900

    indexController에서 반복되는 세션 값 관련 코드를 세션 메소드 인자로 세션값을 받을 수 있도록 설정

---
+++ src/main/java/com/sbr/springboot/config/WebConfig/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sbr.springboot.config.WebConfig;
+
+import com.sbr.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer { //LoginUserArgumentResolver가 스프링에서 인식 될 수 있도록 ,,
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
