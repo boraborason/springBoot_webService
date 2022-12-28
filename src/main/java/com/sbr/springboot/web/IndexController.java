60234ac6dfc9d522c6b94f24ad311698a33272da
boraborason <57280699+boraborason@users.noreply.github.com>
2022-12-28 22:58:21 +0900

    세션 저장소를 DB로 교체

--- src/main/java/com/sbr/springboot/web/IndexController.java
+++ src/main/java/com/sbr/springboot/web/IndexController.java
@@ -1,6 +1,6 @@
 package com.sbr.springboot.web;
 
-
+import com.sbr.springboot.config.auth.LoginUser;
 import com.sbr.springboot.config.auth.dto.SessionUser;
 import com.sbr.springboot.service.PostsService;
 import com.sbr.springboot.web.dto.PostsResponseDto;
@@ -19,10 +19,10 @@ public class IndexController { //뷰 관련
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    @GetMapping("/")
-    public String index(Model model) { //Model -> postsService.findAllDesc()가져온 결과를 posts로 index.머스테치로 전달
+    @GetMapping("/")                  //어느 컨트롤러든지 @LoginUser 만 사용하면 세션 정보를 가져올 수 있게 된다.
+    public String index(Model model,  @LoginUser SessionUser user) { //Model -> postsService.findAllDesc()가져온 결과를 posts로 index.머스테치로 전달
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");//CustomOAuth2UserService에서 로그인 성공시 세션에
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");//CustomOAuth2UserService에서 로그인 성공시 세션에
                                                                                 // SessionUser를 저장
 
         if (user != null) { //세션에 지정된 값이 있을 때만 model에 userName으로 등록
